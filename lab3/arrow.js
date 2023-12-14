7b38812eec74d8c0459136155488c8909fb15f8d
athMZ <s59091@student.ath.edu.pl>
2023-12-14 16:53:39 +0100

    Lab 3, zad 10

---
+++ lab3/arrow.js
@@ -0,0 +1,3 @@
+//10. Zaimplementuj funkcję w postaci „strzałek” (dowolną)
+var sum = function (a, b) { return a + b; };
+console.log(sum(2, 3));
---
+++ lab3/arrow.ts
@@ -0,0 +1,4 @@
+//10. Zaimplementuj funkcję w postaci „strzałek” (dowolną)
+let sum = (a: number, b: number) => a + b;
+
+console.log(sum(2, 3));
